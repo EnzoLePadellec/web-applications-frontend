a9cea2f0879a42bd548cef277deb7fe96775c791
Enzo LE PADELLEC <24466@student.dorset-college.ie>
2023-02-17 14:30:59 +0100

    lien déploiement

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,6 +1,6 @@
 import {error, redirect} from '@sveltejs/kit';
 
-const base = 'http://localhost:3000';
+const base = 'https://secure-web-dev-backend-ufl8.onrender.com';
 
 async function send({ method, path, data, token }) {
     const opts = { method, headers: {} };
